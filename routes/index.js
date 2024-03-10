fb38b014ac20843ed6e5cfb22b72c0cd116f8be1
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2024-03-11 01:35:34 +0200

    added messages list in / route, fixed sending msgs 404 route not found

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,26 @@
+var express = require('express');
+var router = express.Router();
+const messages = require('../data/messages'); // Import messages array
+
+/* GET home page. */
+router.get('/', (req, res) => {
+  console.log('messages', messages);
+  res.render('index', { title: 'Welcome to Mini Messageboard', messages: messages }); // Pass messages array to the template
+});
+
+router.get('/new', (req, res) => {
+  res.render('form', { title: 'Mini Messageboard', messages: messages }); // Pass messages array to the template
+});
+
+/* POST new message. */
+router.post('/new', (req, res) => {
+  const { text, user } = req.body;
+  messages.push({ text, user, added: new Date() });
+  res.redirect('/');
+});
+
+module.exports = { router, messages };
+// routes/index.js
 // var express = require('express');
 // var router = express.Router();
 // const messages = require('../data/messages'); // Import messages array
@@ -7,26 +30,4 @@
 //   res.render('index', { title: 'Welcome to Mini Messageboard', messages: messages }); // Pass messages array to the template
 // });
 
-// router.get('/new', (req, res) => {
-//   res.render('form', { title: 'Mini Messageboard', messages: messages }); // Pass messages array to the template
-// });
-
-// /* POST new message. */
-// router.post('/new', (req, res) => {
-//   const { text, user } = req.body;
-//   messages.push({ text, user, added: new Date() });
-//   res.redirect('/');
-// });
-
 // module.exports = router;
-// routes/index.js
-var express = require('express');
-var router = express.Router();
-const messages = require('../data/messages'); // Import messages array
-
-/* GET home page. */
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Welcome to Mini Messageboard', messages: messages }); // Pass messages array to the template
-});
-
-module.exports = router;
